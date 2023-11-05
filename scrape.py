fc637066edcafd82aee5d6dff3817e6f83451071
imregemes <95701387+Dzsimi@users.noreply.github.com>
2023-11-05 01:09:40 +0100

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -333,8 +333,8 @@ for i in index:
     except Exception as e:
         print("An exception occurred:", e)
         
-if len(df) < 2:
-    raise ValueError("DataTable length is less than 10")
+#if len(df) < 2:
+#    raise ValueError("DataTable length is less than 10")
 
 # Close the WebDriver
-#driver.quit()
+driver.quit()
